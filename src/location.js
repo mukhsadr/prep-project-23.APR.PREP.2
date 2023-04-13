9c30c8671c44f0d45893d30fc21f7bf451eb2d41
Alisha Maddy <alisha831maddy@gmail.com>
2023-04-13 09:15:34 -0700

    get coordinates from location.js

--- src/location.js
+++ src/location.js
@@ -1,3 +1,5 @@
+import './App';
+
 const successCallback = (position) => {
     console.log(position);
   };
@@ -7,3 +9,12 @@ const successCallback = (position) => {
   };
   
   navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+  export default function getCoords() {
+  var coords = navigator.geolocation.getCurrentPosition(function(position){
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    return [lat, long]
+  })
+    return coords
+}
