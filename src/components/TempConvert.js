e5606fe9db00e82ef3eecc938ad1206d1b11b315
JuliaS96 <76749862+JuliaS96@users.noreply.github.com>
2023-04-14 20:37:39 -0700

    fixed unit issue

--- src/components/TempConvert.js
+++ src/components/TempConvert.js
@@ -1,31 +1,27 @@
 import React from "react";
 import { useState } from "react";
 import {FormControlLabel, Switch} from "@mui/material";
-//import { useEffect } from "react";
 
 export default function TempConvert(props) {
-  const [temp, setTemp] = useState(props.currTemp);
   const [unit, setUnit] = useState("C");
   const [label, setLabel] = useState("Convert to °F")
 
-  const oppositeUnit = unit === "C" ? "F" : "C";
-  //setTemp(currTemp);
   const convert = () => {
     console.log(props.currTemp)
     let newT = 0
-    setTemp(props.currTemp)
+    let newUnit = "c"
     if (unit === "C") {
       newT = props.currTemp * 1.8 + 32;
-      setTemp(Math.round(newT));
+      newUnit = "F"
       setUnit("F");
       setLabel("Convert to °C")
     } else if (unit === "F") {
       newT = ((props.currTemp - 32) * 5) / 9;
-      setTemp(Math.round(newT));
+      newUnit = "C"
       setUnit("C");
       setLabel("Convert to °F")
     }
-    props.tempHandler(newT, unit);
+    props.tempHandler(newT, newUnit);
   };
 
   return (
