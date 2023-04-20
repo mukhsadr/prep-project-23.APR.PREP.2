7affa5969d9311a037a71cf2bbddb9662411e1f9
Alisha Maddy <alisha831maddy@gmail.com>
2023-04-20 09:39:50 -0700

    fixed temp conversion in weekly forecast

--- src/components/WeeklyForecast/WeeklyForecast.jsx
+++ src/components/WeeklyForecast/WeeklyForecast.jsx
@@ -1,8 +1,9 @@
 import { useWeatherContext } from "../../store/WeatherContext";
 import "./WeeklyForecast.css";
+import TopBar from "../TopBar";
 
 function WeeklyForecast() {
-  const { weeklyForecast } = useWeatherContext();
+  const { weeklyForecast, temp, unit} = useWeatherContext();
   console.log("WeeklyForecast: ", weeklyForecast)
   
   const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -13,7 +14,7 @@ function WeeklyForecast() {
         <div key={day.date} className="weekly-forecast-card">
           <h3>{weekdays[new Date(day.date).getDay()]}</h3>
           <img src={`http://openweathermap.org/img/w/${day.icon}.png`} alt={day.description} />
-          <div>{day.temp}°C</div>
+          <div>{temp.toFixed(2)}°{unit}</div>
         </div>
       ))}
     </div>
